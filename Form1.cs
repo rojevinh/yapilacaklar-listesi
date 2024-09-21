857997432bc860813a5c3a4543b7d55fd71c6cbb
rojevinh <115618069+rojevinh@users.noreply.github.com>
2024-09-21 15:43:33 +0300

    c# ile yapılacaklar listesi
    form uygulaması ile basit bir to do list

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace todolisstform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+    }
+}
